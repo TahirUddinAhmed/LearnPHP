899ae7675ee47323ed44fa95b7021ac27f3883b4
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-18 16:36:54 +0530

    Solve Array Questins

--- 02-arrays-and-iteration/05-array-challenges/index.php
+++ 02-arrays-and-iteration/05-array-challenges/index.php
@@ -8,7 +8,14 @@
   4. Get the amount of numbers in the array and put into a variable.
   5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
 */
+$numbers = [23, 11, 7, 4, 101, 77, 43];
+$ArrSum = 0;
+for($i = 0; $i < count($numbers); $i++) {
+  $ArrSum += $numbers[$i];
+}
 echo '<h3>Sum Of An Array</h3>';
+echo $ArrSum;
+
 
 /*
   Challenge 2: Colors array
@@ -20,8 +27,17 @@ echo '<h3>Sum Of An Array</h3>';
 
 You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'purple']
 */
+$colors = ['yellow', 'pink', 'blue', 'red', 'purple'];
+rsort($colors);
+array_push($colors, 'orange');
+
+array_splice($colors, 1, 1, 'pink');
+array_pop($colors);
 
 echo '<h3>Colors Array</h3>';
+echo '<pre>';
+echo print_r($colors);
+echo '</pre>';
 
 $colors = ['red', 'blue', 'green', 'yellow'];
 
@@ -33,5 +49,19 @@ $colors = ['red', 'blue', 'green', 'yellow'];
   3. Print out the job_title of the second job listing in the array.
   4. Print out the first skill of the third job listing in the array.
 */
+$jobListings = [
+  ['ID' => 1 ,'Job_title' => 'Front-End Developer', 'Company' => 'Info Teach', 'contact_email' => 'infoteach@gmail.com', 'contact_phone' => 9365910717, 'skill' => ['HTML', 'CSS', 'JS']],
+  ['ID' => 1 ,'Job_title' => 'Back-End Developer', 'Company' => 'Info Teach', 'contact_email' => 'infoteach@gmail.com', 'contact_phone' => 9365910717, 'skill' => ['PHP', 'Laravel'] ],
+  ['ID' => 1 ,'Job_title' => 'Data Science Developer', 'Company' => 'Info Teach', 'contact_email' => 'infoteach@gmail.com', 'contact_phone' => 9365910717, 'skill' => ['Mysql', 'NoSQL'] ],
+  ['ID' => 1 ,'Job_title' => 'App Developer', 'Company' => 'Info Teach', 'contact_email' => 'infoteach@gmail.com', 'contact_phone' => 9365910717, 'skill' => ['JAVA', 'KOTLIN'] ]
+];
+
+$jobListings[] = ['ID' => 1 ,'Job_title' => 'Software Developer', 'Company' => 'Info Teach', 'contact_email' => 'infoteach@gmail.com', 'contact_phone' => 9365910717, 'skill' => ['DSA', 'Java'] ];
 
 echo '<h3>Job Listings</h3>';
+echo 'title of the second job: ' . $jobListings[1]['Job_title'];
+echo '<br>';
+echo 'First skill of the third job is: ' . $jobListings[2]['skill'][0];
+echo '<pre>';
+print_r($jobListings);
+echo '</pre>';
