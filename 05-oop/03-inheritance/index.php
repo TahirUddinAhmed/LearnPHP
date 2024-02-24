b4a40ecb5722cc6eb44bde033d7d756584b679ea
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-24 14:48:56 +0530

    Inheritance

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -1,19 +1,46 @@
 <?php
 
-class User
-{
+class User {
   public $name;
   public $email;
   protected $status = 'active';
 
-  public function __construct($name, $email)
-  {
+  public function __construct($name, $email) {
     $this->name = $name;
     $this->email = $email;
   }
 
-  public function login()
-  {
+  public function login() {
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User {
+  public $level;
+
+  public function __construct($name, $email, $level) {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+
+  // override login method - polymorphism
+  public function login() {
+    echo 'Admin ' . $this->name . ' Logged in <br>';
+  }
+
+  public function getStatus() {
+    echo $this->status;
+  }
+}
+
+
+
+
+
+$admin1 = new Admin('Peter Parker', 'peter@gmail.com', 5);
+echo $admin1->name . "<br>";
+echo $admin1->email . "<br>";
+echo $admin1->level . "<br>";
+echo $admin1->getStatus() . "<br>";
+$admin1->login();
+var_dump($admin1);
