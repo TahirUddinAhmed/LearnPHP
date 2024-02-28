35cc54273d9f2b5a35643862718f73b66e81e9de
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-28 21:11:22 +0530

    alert message challenge

--- 06-superglobals/07-alert-challenge/index.php
+++ 06-superglobals/07-alert-challenge/index.php
@@ -2,12 +2,22 @@
 $title = '';
 $description = '';
 $submitted = false;
+$messages = array();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
   $title = htmlspecialchars($_POST['title'] ?? '');
   $description = htmlspecialchars($_POST['description'] ?? '');
 
+  if(empty($title)) {
+    $messages[] = ['text' => 'Title is required', 'color' => 'text-red-500'];
+    $submitted = false;
+  }
 
+  if(empty($description)) {
+    $messages[] = ['text' => 'Description is required', 'color' => 'text-red-500'];
+    $submitted = false;
+  }
+ 
   $file = $_FILES['logo'];
 
   if ($file['error'] === UPLOAD_ERR_OK) {
@@ -30,16 +40,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     if (in_array($fileExtension, $allowedExtensions)) {
       // Upload file
       if (move_uploaded_file($file['tmp_name'], $uploadDir .  $filename)) {
-        echo 'File Uploaded!';
+        $messages[] = ['text' => 'File Uploaded successfully', 'color' => 'text-green-500'];
+        $submitted = true;
       } else {
-        echo 'File Upload Error: ' . $file['error'];
+        $messages[] = ['text' => 'File Upload Error', 'color' => 'text-red-500'];
+        // echo 'File Upload Error: ' . $file['error'];
       }
     } else {
-      echo 'Invalid File Type';
+      $messages[] = ['text' => 'File must be an image', 'color' => 'text-red-500'];
     }
   }
-
-  $submitted = true;
 }
 ?>
 
@@ -57,6 +67,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
   <div class="flex justify-center items-center h-screen">
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Create Job Listing</h1>
+      <?php foreach($messages as $msg) : ?>
+        <p class="<?= $msg['color'] ?> mb-2">
+          <?= $msg['text'] ?>
+        </p>
+      <?php endforeach; ?>
       <form method="post" enctype="multipart/form-data">
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
